31d4d7258bab7f64fdb544c9e500dab4ff91b682
Ivan Krivyakov <ivan@ikriv.com>
2018-02-12 00:29:14 -0500

    Added UserName type

--- SemanticTypesRun/Program.cs
+++ SemanticTypesRun/Program.cs
@@ -5,6 +5,9 @@ namespace SemanticTypesRun
 {
     class Program
     {
+        enum UserName {}
+
+
         public static void Send(Email email)
         {
             if (email == "ivan@example.com".AsEmail())
@@ -16,10 +19,16 @@ namespace SemanticTypesRun
             Console.WriteLine("Sending email to " + email.Value);
         }
 
+        private static void Login(SString<UserName> user)
+        {
+            Console.WriteLine("Login user: " + user.Value);
+        }
+
         static void Main(string[] args)
         {
             Send("ivan@example.com".AsEmail());
             Send("bla@foobar.com".AsEmail());
+            Login("ivan".As<UserName>());
         }
     }
 }
